14889db7888d07d83ee9504447d405aa0770fc86
HKandulla <hannes@kandulla.de>
2020-08-14 15:31:43 +0200

    test-folder, setup added

---
+++ tests/Serializer/Serializer/MailjetMailSerializerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Faibl\MailjetBundle\Tests\Serializer\Serializer;
+
+use PHPUnit\Framework\TestCase;
+
+class MailjetMailSerializerTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true, 'todo..');
+    }
+}
---
+++ tests/Services/MailjetServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Faibl\MailjetBundle\Tests\Services;
+
+use PHPUnit\Framework\TestCase;
+
+class MailjetServiceTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true, 'todo..');
+    }
+}
